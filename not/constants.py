897e71ae87eeb46cb3dc5398680f024c8f3acfd6
Ainsley McGrath <a.mcgrath@dickblick.com>
2020-04-10 00:26:07 -0500

    some things do not change

--- not/constants.py
+++ not/constants.py
@@ -1,2 +1,6 @@
-VALID_TASK_SPEC_EXTENSION_NAMES = ["yml", "yaml", "not"]
-DOT_NOTHING_DIRECTORY_NAME = ".nothings"
+"""Unchanging values that would be inappropriate for config"""
+from typing import List
+
+VALID_TASK_SPEC_EXTENSION_NAMES: List = ["yml", "yaml", "not"]
+DOT_NOTHING_DIRECTORY_NAME: str = ".nothing"
+STEP_SEPARATOR: str = "\n\n"
