2ad96f74d962b8d59a8357e72acbd3e1e3725ba6
Ainsley McGrath <a.mcgrath@dickblick.com>
2020-05-15 20:34:55 -0500

    enum and pattern

--- not/constants.py
+++ not/constants.py
@@ -1,8 +1,17 @@
 """Unchanging values that would be inappropriate for config"""
+from enum import Enum
+import re
 from typing import List, Set
 
 VALID_TASK_SPEC_EXTENSION_NAMES: List = ["yml", "yaml", "not"]
 DOT_NOTHING_DIRECTORY_NAME: str = ".nothing"
 STEP_SEPARATOR: str = "\n\n"
+TASK_SPEC_EXT_PATTERN = re.compile(r"(\.not|\.yml|\.yaml)$")
 
 FIELD_NAMES_EXCLUDED_FROM_CLEANED_TASK_SPEC: Set = {"filename"}
+
+
+class DirectoryChoicesForListCommand(str, Enum):
+    cwd = "cwd"
+    home = "home"
+    both = "both"
--- not/filesystem.py
+++ not/filesystem.py
@@ -1,13 +1,12 @@
 """filesystem utilities for not"""
 from itertools import chain
 from pathlib import Path
-from typing import Dict, Iterator, Literal, List, Union
+from typing import Iterator, Union
 
 from .constants import (
     DOT_NOTHING_DIRECTORY_NAME,
     VALID_TASK_SPEC_EXTENSION_NAMES,
 )
-from .models import TaskSpec
 
 
 def glob_each_extension(
@@ -33,11 +32,11 @@ def task_spec_location(task_spec_name: str) -> Union[Path, None]:
         path.glob(DOT_NOTHING_DIRECTORY_NAME) for path in [Path.cwd(), Path.home()]
     )
 
-    task_specs_in_home_dot_nothing_dir: Iterator[Iterator] = (
+    task_specs_in_home_dot_nothing_dir: Iterator[Path] = chain(
         glob_each_extension(task_spec_name, path) for path in home_dot_nothing_dir
     )
 
-    task_specs_below_cwd_dot_nothing_dir: Iterator[Iterator] = (
+    task_specs_below_cwd_dot_nothing_dir: Iterator[Path] = chain(
         glob_each_extension(task_spec_name, path, recurse=True)
         for path in cwd_dot_nothing_dir
     )
@@ -51,44 +50,3 @@ def task_spec_location(task_spec_name: str) -> Union[Path, None]:
     )
 
     return next(any_place_the_task_spec_could_be, None)
-
-
-def cwd_task_spec_file_names_by_location() -> Dict[
-    Literal["cwd_dot_nothing_dir", "cwd_dot_not_files"], List[TaskSpec]
-]:
-    """Return TaskSpec objects for each task spec file
-    in [cwd]/.nothing and/or any [cwd]/*.not files."""
-
-    cwd_dot_nothing_dir = next(Path.cwd().glob(DOT_NOTHING_DIRECTORY_NAME), None)
-
-    cwd_task_spec_names: List = (
-        [  # preserve task spec file names wih literal . in them
-            ".".join(path.name).split(".")
-            for path in glob_each_extension("*", cwd_dot_nothing_dir)
-        ]
-        if cwd_dot_nothing_dir is not None
-        else []
-    )
-
-    cwd_dot_not_file_names: List = [
-        ".".join(path.name).split(".") for path in Path.cwd().glob(f"**/*.not")
-    ]
-
-    return {
-        "cwd_dot_nothing_dir": cwd_task_spec_names,
-        "cwd_dot_not_files": cwd_dot_not_file_names,
-    }
-
-
-def home_dot_nothing_dir_task_spec_names() -> Dict[
-    Literal["home_dot_nothing_dir"], List[TaskSpec]
-]:
-    """Return TaskSpec objects for each task spec file under ~/.nothing."""
-    home_dot_nothing_dir = Path.home() / DOT_NOTHING_DIRECTORY_NAME
-
-    home_dot_nothing_dir_task_spec_file_names: List = [
-        ".".join(path.name).split(".")
-        for path in glob_each_extension("*", home_dot_nothing_dir)
-    ]
-
-    return {"home_dot_nothing_dir": home_dot_nothing_dir_task_spec_file_names}
